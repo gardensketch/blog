ea97ff511b189541d045f4eab8e8d9bddecba2de
Tylor Sherman <tylor@denimandsteel.com>
2014-05-29 11:57:22 -0700

    Gardens theme, gitignore, and htaccess

---
+++ wp-content/themes/gardens/footer.php
@@ -0,0 +1,10 @@
+      </div>  <!-- end #main-area -->
+
+    <footer id="main_footer" class="clearfix">
+      <p id="copyright"><?php printf( __('&copy; 2014 %s', 'Flexible'), 'Sky Mark Media' ); ?></p>
+    </footer> <!-- end #main_footer -->
+  </div> <!-- end #container -->
+
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/gardens/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+remove_action( 'et_header_menu', 'et_add_mobile_navigation' );
